5ab773c225c081f0a8887a96edef7d18d4c9246f
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-07-21 22:52:37 +0300

    feat: add file service

---
+++ Src/TMS.Nbrb.Core/Services/FileService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
+using TMS.Nbrb.Core.Helpers;
+
+namespace TMS.Nbrb.Core.Services
+{
+    public class FileService
+    {
+        public void WriteToFileAsync(string text)
+        {
+            WriteAsync(text, Constants.path);
+        }
+
+        public void WriteToFileAsync(string text, string path)
+        {
+            WriteAsync(text, path);
+        }
+
+        private async void WriteAsync (string text, string path)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true, Encoding.Default))
+                {
+                    await sw.WriteLineAsync(text);
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Data was sucssesful write in file");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //ToDo: Make feature to read the file
+    }
+}
