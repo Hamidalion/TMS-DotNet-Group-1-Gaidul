de66b49cc2dffa4af30acc63b7267d1b8b6f3b76
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-22 19:34:33 +0300

    feat: add request service

---
+++ Src/TMS.Nbrb.Core/Interfaces/IRequestService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMS.Nbrb.Core.Models;
+
+namespace TMS.Nbrb.Core.Interfaces
+{
+    interface IRequestService
+    {
+        Task<Currency> GetCurrencyAsync(string code);
+        Task<IEnumerable<Currency>> GetAllCurrenciesAsync();
+    }
+}
---
+++ Src/TMS.Nbrb.Core/Services/RequestService.cs
@@ -0,0 +1,29 @@
+﻿using Flurl;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMS.Nbrb.Core.Interfaces;
+using TMS.Nbrb.Core.Models;
+
+namespace TMS.Nbrb.Core.Services
+{
+    class RequestService : IRequestService
+    {
+        public async Task<Currency> GetCurrencyAsync (string code)
+        {
+            var response = await "https://www.nbrb.by/api/exrates/currencies"
+                .AppendPathSegment(code)
+                .GetJsonAsync<Currency>();
+            return response;
+        }
+
+        public async Task<IEnumerable<Currency>> GetAllCurrenciesAsync()
+        {
+            var response = await "https://www.nbrb.by/api/exrates/currencies"
+                .GetJsonAsync<List<Currency>>();
+            return response;
+        }
+    }
+}
