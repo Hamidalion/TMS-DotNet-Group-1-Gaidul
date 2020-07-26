0062e9f95e151c099484d6e2e969bd5eab7c5779
Dakamakat <gazon-ozon@yandex.ru>
2020-07-26 21:15:12 +0300

    feat: added exception handling

--- Src/TMS.Nbrb.Core/Services/RequestService.cs
+++ Src/TMS.Nbrb.Core/Services/RequestService.cs
@@ -2,7 +2,6 @@
 using Flurl.Http;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using TMS.Nbrb.Core.Interfaces;
 using TMS.Nbrb.Core.Models;
@@ -11,60 +10,133 @@ namespace TMS.Nbrb.Core.Services
 {
     public class RequestService : IRequestService
     {
-        public async Task<Currency> GetCurrencyAsync (string code)
+        public async Task<Currency> GetCurrencyAsync(string code)
         {
-            var response = await "https://www.nbrb.by/api/exrates/currencies"
-                .AppendPathSegment(code)
-                .GetJsonAsync<Currency>();
-            return response;
+            try
+            {
+                var response = await "https://www.nbrb.by/api/exrates/currencies"
+                                .AppendPathSegment(code)
+                                .GetJsonAsync<Currency>();
+                return response;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public async Task<IEnumerable<Currency>> GetAllCurrenciesAsync()
         {
-            var response = await "https://www.nbrb.by/api/exrates/currencies"
+            try
+            {
+                var response = await "https://www.nbrb.by/api/exrates/currencies"
                 .GetJsonAsync<List<Currency>>();
-            return response;
+                return response;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+
         }
 
         public async Task<Rate> GetRateAsync(string code)
         {
-            var response = await "https://www.nbrb.by/api/exrates/rates"
-                .AppendPathSegment(code)
-                .GetJsonAsync<Rate>();
-            return response;
+            try
+            {
+                var response = await "https://www.nbrb.by/api/exrates/rates"
+                               .AppendPathSegment(code)
+                               .GetJsonAsync<Rate>();
+                return response;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public async Task<IEnumerable<Rate>> GetAllRatesAsync()
         {
-            var response1 = await "https://www.nbrb.by/api/exrates/rates"
-                .SetQueryParam("periodicity", 0)
-                .GetJsonAsync<List<Rate>>();
-            var response2 = await "https://www.nbrb.by/api/exrates/rates"
-                .SetQueryParam("periodicity", 1)
-                .GetJsonAsync<List<Rate>>();
-            response1.AddRange(response2);
-            return response1;
+            try
+            {
+                var response1 = await "https://www.nbrb.by/api/exrates/rates"
+                                .SetQueryParam("periodicity", 0)
+                                .GetJsonAsync<List<Rate>>();
+                var response2 = await "https://www.nbrb.by/api/exrates/rates"
+                    .SetQueryParam("periodicity", 1)
+                    .GetJsonAsync<List<Rate>>();
+                response1.AddRange(response2);
+                return response1;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public async Task<Rate> GetRateByDateAsync(string code, DateTime date)
         {
-            var response = await "https://www.nbrb.by/api/exrates/rates"
-                .AppendPathSegment(code)
-                .SetQueryParam("ondate", date.ToString("yyyy-MM-dd"))
-                .GetJsonAsync<Rate>();
-            return response;
+            try
+            {
+                var response = await "https://www.nbrb.by/api/exrates/rates"
+               .AppendPathSegment(code)
+               .SetQueryParam("ondate", date.ToString("yyyy-MM-dd"))
+               .GetJsonAsync<Rate>();
+                return response;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public async Task<IEnumerable<Rate>> GetAllRatesByDateAsync(DateTime date)
         {
-            var response1 = await "https://www.nbrb.by/api/exrates/rates"
-                .SetQueryParams(new { ondate = date.ToString("yyyy-MM-dd"), periodicity = 0 })
-                .GetJsonAsync<List<Rate>>();
-            var response2 = await "https://www.nbrb.by/api/exrates/rates"
-                .SetQueryParams(new { ondate = date.ToString("yyyy-MM-dd"), periodicity = 1 })
-                .GetJsonAsync<List<Rate>>();
-            response1.AddRange(response2);
-            return response1;
+            try
+            {
+                var response1 = await "https://www.nbrb.by/api/exrates/rates"
+                                .SetQueryParams(new { ondate = date.ToString("yyyy-MM-dd"), periodicity = 0 })
+                                .GetJsonAsync<List<Rate>>();
+                var response2 = await "https://www.nbrb.by/api/exrates/rates"
+                    .SetQueryParams(new { ondate = date.ToString("yyyy-MM-dd"), periodicity = 1 })
+                    .GetJsonAsync<List<Rate>>();
+                response1.AddRange(response2);
+                return response1;
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                Console.WriteLine("Request timed out ");
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
     }
 }
